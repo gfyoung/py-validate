3a9ee5f5e068600fe113fedf7521c166321c849f
gfyoung <gfyoung17@gmail.com>
2017-05-26 17:15:22 -0400

    Make py-validate repository a package

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name="py_validate",
+    version="0.0.1",
+    packages=["py_validate",
+              "py_validate.tests"],
+    include_package_data=True,
+    license="MIT License",
+    description="Python function validators",
+    long_description="py-validate is a library that provides easy-to-use\n"
+                     "wrappers for checking inputs and outputs of a function\n"
+                     "without cluttering implementation with verification\n"
+                     "checks. Wrappers nicely abstract away these checks\n"
+                     "and are very customizable for each function.",
+    download_url="https://github.com/gfyoung/py-validate",
+    url="https://github.com/gfyoung/py-validate",
+    author="G. Young",
+    author_email="None - use the homepage instead!",
+    bugtrack_url="https://github.com/gfyoung/py-validate/issues",
+    keywords="python validate wrapper function",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Pre-processors",
+    ]
+)
