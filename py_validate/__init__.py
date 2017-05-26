144e69d73a83a60c58640d9029cf98477711e1bd
gfyoung <gfyoung17@gmail.com>
2017-05-26 17:54:27 -0400

    Add testing utility in __init__.py file

--- py_validate/__init__.py
+++ py_validate/__init__.py
@@ -1,1 +1,13 @@
 from py_validate.validator import *  # noqa
+
+from subprocess import call
+from os.path import dirname
+
+
+def test():
+    """
+    Run unit tests on the current py_validate installation.
+    """
+
+    directory = dirname(__file__)
+    call(["pytest", directory])
